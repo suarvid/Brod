ea977d1c9cf6ec32617d8dfec99b8578711042ee
Arvid Sundbom <arvid.sundbom98@gmail.com>
2023-05-25 00:19:48 +0200

    Now possible to return values in vector from macro invocation

--- examples/sync_macro.rs
+++ examples/sync_macro.rs
@@ -1,5 +1,5 @@
 use brod::prod_utils;
-use brod::spawn_threads_with_args;
+use brod::produce_parallel;
 use rdkafka::producer::BaseProducer;
 use rdkafka::producer::BaseRecord;
 
@@ -13,13 +13,32 @@ fn publish_some_stuff(
     times_to_publish: i32,
     key: String,
     message: String,
-) {
+) -> u32 {
     for i in 0..times_to_publish {
         match prod.send(BaseRecord::to(topic).key(&key).payload(&message)) {
             Ok(_) => println!("Successfully sent message {}", i),
             Err(_) => println!("Failed to send message"),
         }
     }
+
+    1337
+}
+
+fn publish_some_other_stuff(
+    prod: BaseProducer,
+    topic: &str,
+    times_to_publish: i32,
+    key: String,
+    message: String,
+) -> usize {
+    for i in 0..times_to_publish {
+        match prod.send(BaseRecord::to(topic).key(&key).payload(&message)) {
+            Ok(_) => println!("Successfully sent message {}", i),
+            Err(_) => println!("Failed to send message"),
+        }
+    }
+
+    1337
 }
 
 fn main() {
@@ -29,7 +48,18 @@ fn main() {
     let producer_config = &prod_utils::get_default_producer_config("localhost:9092", "100");
 
     //spawn_threads_with_args!(num_threads; producer_config; add, 1, 2; 3, 4);
-    spawn_threads_with_args!(num_threads; producer_config; publish_some_stuff,
+    let results = produce_parallel!(num_threads; producer_config; publish_some_stuff,
         topic, 5, String::from("Henlo"), String::from("Henlo");
         topic, 100, String::from("Baja"), String::from("Maja"));
+
+    for r in results {
+        println!("r: {:#?}", r);
+    }
+
+    let results = produce_parallel!(num_threads; producer_config; publish_some_other_stuff,
+        topic, 5, String::from("A"), String::from("B"));
+
+    for r in results {
+        println!("r: {:#?}", r);
+    }
 }
--- src/macro_sync_prod.rs
+++ src/macro_sync_prod.rs
@@ -1,11 +1,24 @@
 #[macro_export]
-macro_rules! spawn_threads_with_args {
+/// Macro for executing a worker function, which is expected to produce messages to Kafka, in
+/// parallel.
+///
+/// # Arguments
+///
+/// * `num_threads` - the number of threads to use for execution; an expression
+/// * `prod_config` - the config object used to create producers; an expression
+/// * `func` - a function pointer to the worker function; an expression
+/// * `args` - zero or more repeated expressions, internally separated by a comma, representing the
+/// arguments to be passed to the worker function. Separate calls to the worker function can be
+/// designated by using a semicolon (;) to (externally) separate the arguments used in the
+/// respective calls.
+macro_rules! produce_parallel {
     ($num_threads:expr; $prod_config:expr; $func:expr, $($($args:expr),*);*) => {
         {
             use rdkafka::ClientConfig;
             use rdkafka::producer::BaseProducer;
             use std::sync::mpsc::channel;
             use std::sync::Arc;
+            let mut results = Vec::new();
             $(
                 let mut handles = Vec::new();
                 let prod_config: &ClientConfig = $prod_config;
@@ -19,27 +32,39 @@ macro_rules! spawn_threads_with_args {
                     let tx = tx.clone();
                     let prod_config = prod_config.clone();
                     handles.push(std::thread::spawn(move || {
-                        let producer: BaseProducer = prod_config.create().expect("Producer creation error");
-                        let result = $func(producer, $($args),*);
-                        println!("Thread {:?} result {:?}", std::thread::current().id(), result);
-                        tx.send(result).expect("Failed to send result!");
+                        match prod_config.create::<BaseProducer>() {
+                            Ok(producer) => {
+                                let result = $func(producer, $($args),*);
+                                let send_res = tx.send(result);
+                                if let Err(send_err) = send_res {
+                                    println!("ERROR: Failed to send function result: {:#?}", send_res);
+                                }
+                            },
+                            Err(e) => println!("ERROR: Failed to create producer instance: {:#?}", e),
+                        }
                     }));
-
                 }
 
                 drop(tx);
 
-                let mut results = Vec::new();
-
                 for handle in handles {
-                    let res = rx.recv().expect("Failed to receive result!");
-                    results.push(res);
-                    handle.join().unwrap();
+                    match rx.recv() {
+                        Ok(res) => {
+                            results.push(res);
+                            handle.join().unwrap();
+                        }
+                        Err(e) => {
+                            println!("ERROR: Failed to receive function result: {:#?}", e);
+                            println!("WARNING: Attempting to join thread handle without receving result.");
+                            handle.join().unwrap();
+                        }
+                    }
                 }
 
 
             )*
 
+            results
         }
 
     };
